ed98ba16dab82ae9cb6b58ac1c6c00b6696231bc
Kuba <dsw48782@student.dsw.edu.pl>
2023-06-07 14:41:56 +0200

    Dodano

--- projekt.py
+++ projekt.py
@@ -9,3 +9,11 @@ parser = argparse.ArgumentParser(description='Konwersja plików XML, JSON i YAML
 parser.add_argument('input_file', type=str, help='Nazwa pliku wejściowego.')
 parser.add_argument('output_file', type=str, help='Nazwa pliku wyjściowego.')
 args = parser.parse_args()
+if args.input_file.endwith('.json'):
+    with open(args.input_file, 'r') as j:
+        try:
+            data = json.load(j)
+        except json.JSONDecodeError as e:
+            print('Niepoprawnny format pliku...')
+            exit(1)
+
